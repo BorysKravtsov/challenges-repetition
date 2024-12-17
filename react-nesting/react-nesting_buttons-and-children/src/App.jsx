7a74b2ede8ea7770e2f36d5546e526ae92818e43
Borys <mamkinboris3@gmail.com>
2024-12-17 15:23:24 +0100

    challenge done

--- react-nesting/react-nesting_buttons-and-children/src/App.jsx
+++ react-nesting/react-nesting_buttons-and-children/src/App.jsx
@@ -3,14 +3,15 @@ import "./App.css";
 export default function App() {
   return (
     <main>
-      <Button>h</Button>
-      <Button>e</Button>
-      <Button>l</Button>
-      <Button>p</Button>
+      <Button>Button 1</Button>
+      <Button>Button 2</Button>
+      <Button>Button 3</Button>
+      <Button>Click Me!</Button>
     </main>
   );
 }
 
+// Reusable Button component
 function Button({ children }) {
   return (
     <button className="button" type="button">
